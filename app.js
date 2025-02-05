b4ec264698c896e18a5864f07d09711e4f2b49d6
Juan David <juan.david.cruz.calderon@gmail.com>
2025-02-05 16:15:45 -0500

    Crea función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-let listaAmigos = [];
+let amigos = [];
 
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
     if (nombreAmigo.trim().length === 0) {
         alert('El campo está vacío, digite el nombre nuevamente.');
     } else {
-        listaAmigos.push(nombreAmigo);
-        asignarTextoElemento('ul', `Nombre: ${nombreAmigo} agregado.`);
+        amigos.push(nombreAmigo);
     }
     limpiarCaja();
+    actualizarListaAmigos();
     return;
 }
 
-function sortearAmigo() {
-
+function actualizarListaAmigos() {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+    for (var i = 0; i <= amigos.length - 1; i++) {
+        let elementoDeLista = document.createElement('li');
+        elementoDeLista.textContent = amigos[i];
+        listaHTML.appendChild(elementoDeLista);
+    }
 }
 
 function asignarTextoElemento(elemento, texto) {
